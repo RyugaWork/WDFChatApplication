3c48d2ea88cfd6be8030bf18f3ba53e68f04612f
RyugaWork <buidinhlongwork@gmail.com>
2025-08-08 20:52:34 +0700

    Add project files.

---
+++ WDFChatApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows;
+
+namespace MyApp {
+    class Program {
+        [STAThread] // required for WPF
+        static void Main(string[] args) {
+            Console.WriteLine("Starting WPF window...");
+
+            //var app = new Application();
+            //var window = new MainWindow();
+            //app.Run(window);
+        }
+    }
+}
